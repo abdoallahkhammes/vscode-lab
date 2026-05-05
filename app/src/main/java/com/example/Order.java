75d9c1ae38c9ba0f3974fc69c1813cec35effda3
abdallah khammes <abdoukhammas9@gmail.com>
2026-05-05 13:30:40 +0100

    Add model classes Customer, Item, Order

---
+++ app/src/main/java/com/example/Order.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.List;
+
+public class Order {
+    private Customer customer;
+    private List<Item> items;
+
+    public Order(Customer customer, List<Item> items) {
+        this.customer = customer;
+        this.items = items;
+    }
+
+    public Customer getCustomer() { return customer; }
+    public List<Item> getItems() { return items; }
+}
